a9036bd30b31c03a9d437e1e8c4ba99dfc48c07c
albertyeh5888 <1048576rsk@gmail.com>
2026-05-15 21:51:03 +0800

    修正：處理 Vercel 編譯失敗問題與檔案路徑

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,6 @@ import React, { useState, useRef } from 'react';
 import Webcam from 'react-webcam';
 import html2canvas from 'html2canvas';
 import './App.css';
-<<<<<<< HEAD
-=======
-
-
-import yukawaPortrait from './assets/yukawa-portrait.png';
->>>>>>> 5c3861782be4beeff1f4e3eaade361d67f2fd178
 
 // 匯入素材
 import yukawaPortrait from './assets/yukawa-portrait.png';
@@ -187,4 +181,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;pp;
